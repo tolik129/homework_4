13d03faf62c42348cd0b8b00229a683c72e448a6
Anatoliy <boykoanatoliy@ukr.net>
2014-11-23 00:04:40 +0200

    first commit

---
+++ wp-content/plugins/arzamath_17th/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: arzamath_17th
+Plugin URI:
+Description: Плагін перший.
+Version: 1.0
+Author: Бойко Анатолій
+*/
+?>
---
+++ wp-content/plugins/arzamath_17th/readme.txt
@@ -0,0 +1,5 @@
+=== arzamath_17th ===
+Tags: arzamath_17th
+Donate link: arzamath_17th
+Tested up to: 4.0
+License: 1.0
